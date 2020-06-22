66510f0f7b7914df904c269ea21991bdbc053f98
Michael Russo <russo.mail@gmail.com>
2020-06-22 08:25:40 -0400

    Adds aws-cdk.aws-ec2 to install_requires

--- hello-cdk/setup.py
+++ hello-cdk/setup.py
@@ -20,7 +20,8 @@ setuptools.setup(
 
     install_requires=[
         "aws-cdk.core==1.46.0",
-        "aws-cdk.aws-s3==1.46.0"
+        "aws-cdk.aws-s3==1.46.0",
+        "aws-cdk.aws-ec2==1.46.0"
     ],
 
     python_requires=">=3.6",
